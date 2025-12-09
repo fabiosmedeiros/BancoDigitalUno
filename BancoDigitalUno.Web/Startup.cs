939293e1e57dc6e19fe6dfa194561df43bdd6c6c
Fabio S Medeiros <fabiosmedeiros@gmail.com>
2025-12-09 11:48:08 -0300

    Commit inicial. Criação de estrutura de diretórios e projeto da camada de apresentação.

---
+++ BancoDigitalUno.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BancoDigitalUno.Web.Startup))]
+namespace BancoDigitalUno.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
